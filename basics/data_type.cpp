e31df16ba9255190cdc536478fd9b4937cdd5dea
shashikant kumar <sp8326441@gmail.com>
2026-03-22 21:02:18 +0530

    feat(C++ Basics): add program demonstrating basic data types

---
+++ basics/data_type.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int age = 20;
+    float price = 99.5;
+    char grade = 'A';
+    bool ispassed = true;
+
+    cout << age << endl;
+    cout << price << endl;
+    cout << grade << endl;
+    cout << ispassed << endl;
+
+    return 0;
+}
