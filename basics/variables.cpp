1370096573302f648234daf1bdeec9fdb74800c0
shashikant kumar <sp8326441@gmail.com>
2026-03-22 20:51:48 +0530

    Create variables.cpp

---
+++ basics/variables.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    string name = "shashi";
+    int age = 21;
+
+    cout << "name:" << name << endl;
+    cout << "Age:" << age << endl;
+
+    return 0;
+}
