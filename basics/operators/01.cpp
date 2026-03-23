a0d01f016f241c6348d6cab8ac53b58a246f3253
shashikant kumar <sp8326441@gmail.com>
2026-03-23 20:16:28 +0530

    Create 01.cpp

---
+++ basics/operators/01.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main(){
+    // arithmetic operators
+    int a;
+    cout << "enter a:";
+    cin >> a;
+    
+    int b;
+    cout << "enter b:";
+    cin >> b;
+    
+    cout << "sum: " << a + b << endl;
+    cout << "diffrence: " << a - b << endl;
+    cout << "product: " << a * b << endl;
+    cout <<  "division: " << a / b << endl;
+    cout << "modules: " << a % b << endl;
+
+    return 0;
+}
