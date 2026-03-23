0058b481de39ba93a13df2f57716f91cb4f52a36
shashikant kumar <sp8326441@gmail.com>
2026-03-23 20:23:51 +0530

    Create 01.cpp

---
+++ basics/operators/01.cpp
@@ -0,0 +1,18 @@
+// sum of two numbers
+#include<iostream>
+using namespace std;
+int main(){
+    int a,b;
+
+    cout << "enter a: ";
+    cin >> a;
+
+    cout << "enter b: ";
+    cin >> b;
+
+    int sum = a + b;
+
+    cout << "sum is: " << sum << endl;
+
+    return 0;
+}
