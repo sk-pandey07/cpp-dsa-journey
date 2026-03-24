1876d2e942151b052b6f799a88c9b3e8e631cbff
shashikant kumar <sp8326441@gmail.com>
2026-03-24 20:24:36 +0530

    Create 08.cpp

---
+++ basics/conditional_statement/08.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int num1,num2,num3;
+    cout << "enter num1: ";
+    cin >> num1;
+
+    cout << "enter num2: ";
+    cin >> num2;
+
+    cout << "enter num3: ";
+    cin >> num3;
+
+    if(num1 > num2 && num1 > num3){
+        cout << num1 << " num1 is greater";
+    }
+    else if(num2 > num1 && num2 > num3){
+        cout << num2 << " num2 is greater";
+    }
+    else{
+        cout << num3 << " num3 is greater";
+    }
+
+    return 0;
+}
