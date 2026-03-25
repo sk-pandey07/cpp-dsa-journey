23860510dab8205b528845107b5f7892458f1a8d
shashikant kumar <sp8326441@gmail.com>
2026-03-25 22:53:09 +0530

    Create 01.cpp

---
+++ basics/loops/while_loop/01.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout << "enter number:";
+    cin >> n;
+
+    int rev = 0;
+
+    while(n != 0){
+        int digit = n % 10;
+        rev = rev * 10 + digit;
+        n = n / 10;
+    }
+    cout << "reverse digit: " << rev;
+    return 0;
+}
