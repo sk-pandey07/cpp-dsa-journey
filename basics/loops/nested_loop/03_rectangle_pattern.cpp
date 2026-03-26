cae0cc1aad4004caae72c6b6f16b89961c582069
shashikant kumar <sp8326441@gmail.com>
2026-03-26 23:41:49 +0530

    feat(C++ Patterns): print rectangle star pattern using rows and columns

---
+++ basics/loops/nested_loop/03_rectangle_pattern.cpp
@@ -0,0 +1,18 @@
+// Print Rectangle (4 rows, 5 columns)
+#include <iostream>
+using namespace std;
+int main() {
+    int rows, cols;
+    cout << "Enter number of rows: ";
+    cin >> rows;
+
+    cout << "Enter number of columns: ";
+    cin >> cols;
+    for (int i = 1; i <= rows; i++) {
+        for (int j = 1; j <= cols; j++) {
+            cout << "* ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
