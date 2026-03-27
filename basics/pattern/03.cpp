6be164a1eb6fb7576482579e372d983e04b447d1
shashikant kumar <sp8326441@gmail.com>
2026-03-27 19:40:52 +0530

    Create 03.cpp

---
+++ basics/pattern/03.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout << "enter number:";
+    cin >> n;
+
+    for(int i=1; i<=n; i++){
+        char ch = 'A';
+        for(int j=1; j<=n; j++){
+            cout << ch;
+            ch = ch + 1;
+        }
+        cout << endl;
+    }
+    return 0;
+}
