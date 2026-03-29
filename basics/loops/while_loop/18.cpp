1dcb252bf95fbd618540404a0133101272117ea8
shashikant kumar <sp8326441@gmail.com>
2026-03-29 11:44:40 +0530

    Create 18.cpp

---
+++ basics/loops/while_loop/18.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout << "enter number:";
+    cin >> n;
+
+    int binary = 0, place = 1;
+    while(n > 0){
+        int rem = n % 2;
+        binary = binary + rem * place;
+        place = place * 10;
+        n = n /2;
+    }
+    cout << "binary: " << binary;
+    return 0;
+}
