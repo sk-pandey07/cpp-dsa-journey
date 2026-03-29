fed49d5805a120e0421fae4a5b99d3eec859b67c
shashikant kumar <sp8326441@gmail.com>
2026-03-29 11:37:22 +0530

    Create 16.cpp

---
+++ basics/loops/while_loop/16.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout << "enter number:";
+    cin >> n;
+    if(n <= 1){
+        cout << "not prime";
+    }
+    bool isprime = true;
+    for(int i=2;i<n;i++){
+        if(n % i == 0){
+            isprime = false;
+            break;
+        }
+    }
+    if(isprime){
+        cout << n << " is prime";
+    }
+    else{
+        cout << n << " isn't prime";
+    }
+    return 0;
+}
