9eccb960410c02cd05dc2d4ba83cf17705ad4747
shashikant kumar <sp8326441@gmail.com>
2026-04-05 23:50:54 +0530

    Create 05.cpp

---
+++ basics/Number system practice/05.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int reversebinary(int n){
+    int rev = 0;
+    while(n > 0){
+        int rem = n % 2;
+        rev = rev * 2 + rem;
+        n = n / 2;
+    }
+    return rev;
+}
+int main(){
+    int n;
+    cout << "enter number: ";
+    cin >> n;
+    
+    cout << "reverse binary -> " << reversebinary(n);
+    return 0;
+}
