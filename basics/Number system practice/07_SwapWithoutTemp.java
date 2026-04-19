2012a4cc812266d6c05f2e3d8d946e31a23f603d
shashikant kumar <sp8326441@gmail.com>
2026-04-19 10:09:38 +0530

    feat: swap two numbers using xor without temporary variable in java

--- basics/Number system practice/07_SwapWithoutTemp.java
+++ basics/Number system practice/07_SwapWithoutTemp.java
@@ -1,9 +1,12 @@
-public class Swap {
-    public static void main(String[] args) {
-        int a = 5, b = 3;
-        a = a ^ b;
-        b = a ^ b;
-        a = a ^ b;
-        System.out.println("a=" + a + " b=" + b);
-    }
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a = 5, b = 3;
+    a = a ^ b;
+    b = a ^ b;
+    a = a ^ b;
+
+    cout << "a=" << a << " b=" << b;
+    return 0;
 }
