84254d44da92f5f8a767b0667467dba39769b49b
shashikant kumar <sp8326441@gmail.com>
2026-04-24 22:08:11 +0530

    Create 02.cpp

---
+++ array/creation/Traversal/02.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int arr[] = {89,56,45,99,32};
+    int size = 5;
+
+    int min = arr[0];
+    int max = arr[0];
+
+    for(int i=0; i<size; i++){
+        if(min < arr[i]){
+            min = arr[i];
+        }
+        if(max > arr[i]){
+            max = arr[i];
+        }
+    }
+    cout << "minimum value: " << min << endl;
+    cout << "maximum value: " << max << endl;
+    return 0;
+}
