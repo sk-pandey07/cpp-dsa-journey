04b3b4df21151be64ab133a0ea00a9df4f03b57b
shashikant kumar <sp8326441@gmail.com>
2026-04-24 22:05:26 +0530

    feat: demonstrate array initialization and element access

---
+++ array/creation/array_basic_print.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+    double price[] = {101.3,102.1,44.3};
+
+    int marks[5] = {98,87,89,92,91};
+    cout << marks[0] << endl;
+    cout << marks[1] << endl;
+    cout << marks[2] << endl;
+    cout << marks[3] << endl;
+    cout << marks[4] << endl;
+
+    return 0;
+}
