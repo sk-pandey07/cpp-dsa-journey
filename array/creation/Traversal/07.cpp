78a5c26c5f66672411b72bc921cba589d4844664
shashikant kumar <sp8326441@gmail.com>
2026-04-25 22:34:57 +0530

    Create 07.cpp

---
+++ array/creation/Traversal/07.cpp
@@ -0,0 +1,18 @@
+// Count numbers greater than X
+#include<iostream>
+using namespace std;
+int main(){
+    int arr[] = {10,25,5,30,15};
+    int size = 5;
+
+    int x = 15;
+    int count = 0;
+
+    for(int i=0; i<size; i++){
+        if(arr[i] > 15){
+            count++;
+        }
+    }
+    cout << "count number greater than 15 = " << count;
+    return 0;
+}
