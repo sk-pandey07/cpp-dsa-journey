6376350ebea7b926581d33e60dfa7ed85693e077
shashikant kumar <sp8326441@gmail.com>
2026-04-27 22:28:29 +0530

    Create 14.cpp

---
+++ array/creation/Traversal/14.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int num[] = {12, 345, 2, 6, 7896};
+    int size = 5;
+
+    int count = 0;
+    for(int i=0; i<size; i++){
+        int n = num[i];
+        int digit = 0;
+
+        while(n != 0){
+            digit++;
+            n /= 10;
+        }
+
+        if(digit % 2 == 0){
+            count++;
+        }
+    }
+    cout << "Even number of digit = " << count;
+    return 0;
+}
