47d2db4ef3572e5cf52bcc333eea23163bd1b701
shashikant kumar <sp8326441@gmail.com>
2026-04-28 23:18:59 +0530

    Create 20.cpp

---
+++ array/creation/Traversal/20.cpp
@@ -0,0 +1,14 @@
+// Print elements at odd index
+#include<iostream>
+using namespace std;
+int main(){
+    int num[] = {2,1,4,5,33,6,3,13};
+    int size = 8;
+
+    for(int i=0; i<size; i++){
+        if(i % 2 != 0){
+            cout << num[i] << " ";
+        }
+    }
+    return 0;
+}
