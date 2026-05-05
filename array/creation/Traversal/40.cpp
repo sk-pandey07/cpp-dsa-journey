a21dcdd1edbbf083683f1b7ef71f2d828077a4bd
shashikant kumar <sp8326441@gmail.com>
2026-05-05 22:56:39 +0530

    Update 40.cpp

--- array/creation/Traversal/40.cpp
+++ array/creation/Traversal/40.cpp
@@ -1,12 +1,10 @@
 #include<iostream>
 using namespace std;
-
 int main(){
     int nums[] = {1,1,2};
     int size = 3;
 
     int j = 0;
-
     for(int i = 1; i < size; i++){
         if(nums[i] != nums[j]){
             j++;
@@ -15,13 +13,11 @@ int main(){
     }
 
     int k = j + 1;
-
     cout << "Number of unique elements = " << k << endl;
 
     cout << "Array after removing duplicates: ";
     for(int i = 0; i < k; i++){
         cout << nums[i] << " ";
     }
-
     return 0;
 }
