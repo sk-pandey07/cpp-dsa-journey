54c69af5e573418d49f26245d4e73bcf170fe924
shashikant kumar <sp8326441@gmail.com>
2026-05-09 23:50:42 +0530

    Create 44.cpp

---
+++ array/creation/Traversal/44.cpp
@@ -0,0 +1,18 @@
+// leetcode 53 , bruteforce
+#include<iostream>
+using namespace std;
+int main(){
+    int num[] = {1,2,3,4,5};
+    int n = 5;
+
+    for(int st=0; st<n; st++){
+        for(int end=st; end<n; end++){
+            for(int i=st; i<=end; i++){
+                cout << num[i];
+            }
+            cout << " ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
