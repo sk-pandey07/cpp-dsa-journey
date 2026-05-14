e82e33d8c71e9e1ddbcf6b43824d3a03b3f20a71
shashikant kumar <sp8326441@gmail.com>
2026-05-14 23:47:07 +0530

    Create 03.cpp

---
+++ array/creation/Linear_search/03.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int main(){
+    vector<int> nums = {0,3,2,1};
+    int n = nums.size();
+
+    int i = 0;
+    while(i+1 < n && nums[i] < nums[i+1]){
+        i++;
+    }
+
+    if(i == 0 && i == n-1){
+        cout << "false";
+        return 0;
+    }
+
+    while(i+1 < n && nums[i] > nums[i+1]){
+        i++;
+    }
+
+    if(i == n-1){
+        cout << "true";
+    }
+    else{
+        cout << "false";
+    }
+    return 0;
+}
