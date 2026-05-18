39f1d4df6557009d809add11c11f2a26c2b7b489
shashikant kumar <sp8326441@gmail.com>
2026-05-18 23:18:57 +0530

    Update 07.cpp

--- array/creation/Linear_search/07.cpp
+++ array/creation/Linear_search/07.cpp
@@ -1,23 +1,15 @@
 #include<iostream>
 #include<vector>
-
 using namespace std;
-
 int main(){
-
     vector<int> flowerbed = {1,0,0,0,1};
-
     int n = 1;
-
     int count = 0;
-
     for(int i = 0; i < flowerbed.size(); i++){
-
         if(flowerbed[i] == 0){
-
             bool leftEmpty =
                 (i == 0 || flowerbed[i - 1] == 0);
-
+            
             bool rightEmpty =
                 (i == flowerbed.size() - 1 ||
                  flowerbed[i + 1] == 0);
