49de34cd98de7d334c0d593d076d2865e6310f93
shashikant kumar <sp8326441@gmail.com>
2026-05-19 22:50:41 +0530

    Create 08.cpp

---
+++ array/creation/Linear_search/08.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+int main(){
+
+    vector<int> nums = {1,2,2,3};
+
+    bool increasing = true;
+    bool decreasing = true;
+    for(int i = 0; i < nums.size() - 1; i++){
+
+        if(nums[i] > nums[i + 1]){
+            increasing = false;
+        }
+    }
+
+    // check decreasing
+    for(int i = 0; i < nums.size() - 1; i++){
+
+        if(nums[i] < nums[i + 1]){
+            decreasing = false;
+        }
+    }
+
+    if(increasing || decreasing){
+        cout << "true";
+    }
+    else{
+        cout << "false";
+    }
+
+    return 0;
+}
