c188ac56705064cefc9ac7cee86a83b595936c54
shashikant kumar <sp8326441@gmail.com>
2026-05-19 22:51:21 +0530

    Update 08.cpp

--- array/creation/Linear_search/08.cpp
+++ array/creation/Linear_search/08.cpp
@@ -11,10 +11,7 @@ int main(){
             increasing = false;
         }
     }
-
-    // check decreasing
     for(int i = 0; i < nums.size() - 1; i++){
-
         if(nums[i] < nums[i + 1]){
             decreasing = false;
         }
