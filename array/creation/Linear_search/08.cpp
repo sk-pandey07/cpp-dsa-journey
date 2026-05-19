cbd78e356dfca87f2fd56985fbd8656bf0b29d26
shashikant kumar <sp8326441@gmail.com>
2026-05-19 22:51:04 +0530

    Update 08.cpp

--- array/creation/Linear_search/08.cpp
+++ array/creation/Linear_search/08.cpp
@@ -1,12 +1,8 @@
 #include<iostream>
 #include<vector>
-
 using namespace std;
-
 int main(){
-
     vector<int> nums = {1,2,2,3};
-
     bool increasing = true;
     bool decreasing = true;
     for(int i = 0; i < nums.size() - 1; i++){
