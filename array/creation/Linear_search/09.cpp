632c20711a45f171f7d550ededa44362b6351a4a
shashikant kumar <sp8326441@gmail.com>
2026-05-20 22:40:37 +0530

    Create 09.cpp

---
+++ array/creation/Linear_search/09.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int main(){
+    // brute force aproch
+    vector<int> nums = {3,5};
+    int n = nums.size();
+    for(int x=0; x<=n; x++){
+        int count = 0;
+        for(int i=0; i<n; i++){
+            if(nums[i] >= x){
+                count++;
+            }
+        }
+        if(count == x){
+            cout << "special x = " << x;
+            return 0;
+        }
+    }
+    cout << -1;
+    return 0;
+}
