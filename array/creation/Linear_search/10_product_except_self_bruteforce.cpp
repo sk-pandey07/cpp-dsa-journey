f853d6e2ac3db68eb323ac5a7f4d90a2e963a671
shashikant kumar <sp8326441@gmail.com>
2026-05-21 23:31:06 +0530

    Update 10_product_except_self_bruteforce.cpp

--- array/creation/Linear_search/10_product_except_self_bruteforce.cpp
+++ array/creation/Linear_search/10_product_except_self_bruteforce.cpp
@@ -8,9 +8,7 @@ int main(){
     vector<int> nums = {1,2,3,4};
 
     int n = nums.size();
-
     vector<int> ans;
-
     for(int i = 0; i < n; i++){
         int product = 1;
         for(int j = 0; j < n; j++){
