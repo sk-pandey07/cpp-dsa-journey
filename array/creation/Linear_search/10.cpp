3115c47da7839c9ed4ebb96fd69b75202194ab27
shashikant kumar <sp8326441@gmail.com>
2026-05-21 23:28:55 +0530

    Create 10.cpp

---
+++ array/creation/Linear_search/10.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+int main(){
+
+    vector<int> nums = {1,2,3,4};
+
+    int n = nums.size();
+
+    vector<int> ans;
+
+    for(int i = 0; i < n; i++){
+
+        int product = 1;
+
+        for(int j = 0; j < n; j++){
+
+            if(i != j){
+                product *= nums[j];
+            }
+        }
+
+        ans.push_back(product);
+    }
+
+    cout << "Answer: ";
+    for(int num : ans){
+        cout << num << " ";
+    }
+
+    return 0;
+}
