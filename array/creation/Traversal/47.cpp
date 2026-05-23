47553858e5d6bf242388e854d602987386851f9c
shashikant kumar <sp8326441@gmail.com>
2026-05-23 23:36:04 +0530

    Update 47.cpp

--- array/creation/Traversal/47.cpp
+++ array/creation/Traversal/47.cpp
@@ -4,13 +4,9 @@ using namespace std;
 int main(){
     vector<int> nums = {1,2,1};
     vector<int> ans;
-
-    // first copy
     for(int i = 0; i < nums.size(); i++){
         ans.push_back(nums[i]);
     }
-
-    // second copy
     for(int i = 0; i < nums.size(); i++){
         ans.push_back(nums[i]);
     }
