8d209e55d2b1e7ce231a5742272485995fb706fc
shashikant kumar <sp8326441@gmail.com>
2026-05-24 23:05:49 +0530

    Update 48.cpp

--- array/creation/Traversal/48.cpp
+++ array/creation/Traversal/48.cpp
@@ -5,9 +5,7 @@ using namespace std;
 int main(){
 
     vector<int> nums = {1,2,1};
-
     int n = nums.size();
-
     vector<int> ans(2 * n);
     for(int i = 0; i < n; i++){
         ans[i] = nums[i];
