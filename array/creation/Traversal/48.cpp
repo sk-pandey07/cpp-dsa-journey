d3d259b3e7987944139dbf852f83733cf70f2f6a
shashikant kumar <sp8326441@gmail.com>
2026-05-24 23:04:25 +0530

    Create 48.cpp

---
+++ array/creation/Traversal/48.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+int main(){
+
+    vector<int> nums = {1,2,1};
+
+    int n = nums.size();
+
+    vector<int> ans(2 * n);
+
+    for(int i = 0; i < n; i++){
+
+        ans[i] = nums[i];
+        ans[i + n] = nums[i];
+    }
+    cout << "Concatenated Array: ";
+    for(int num : ans){
+        cout << num << " ";
+    }
+    return 0;
+}
