1df69d46c499da20cdbe53ea88e93994a3d2d048
shashikant kumar <sp8326441@gmail.com>
2026-05-25 23:36:59 +0530

    Update 11.cpp

--- array/creation/Linear_search/11.cpp
+++ array/creation/Linear_search/11.cpp
@@ -3,9 +3,7 @@ public:
     vector<int> findDuplicates(vector<int>& nums) {
 
         vector<int> ans;
-
         int n = nums.size();
-
         for(int i = 0; i < n; i++){
             int freq = 0;
             for(int j = 0; j < n; j++){
