c20c34f4eec421e32279dc2dbc3265f2001a45ef
shashikant kumar <sp8326441@gmail.com>
2026-05-25 23:35:44 +0530

    Create 11.cpp

---
+++ array/creation/Linear_search/11.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    vector<int> findDuplicates(vector<int>& nums) {
+
+        vector<int> ans;
+
+        int n = nums.size();
+
+        for(int i = 0; i < n; i++){
+
+            int freq = 0;
+
+            // count frequency
+            for(int j = 0; j < n; j++){
+
+                if(nums[i] == nums[j]){
+                    freq++;
+                }
+            }
+
+            // duplicate found
+            if(freq == 2){
+
+                bool alreadyAdded = false;
+
+                // avoid duplicate push
+                for(int x : ans){
+
+                    if(x == nums[i]){
+                        alreadyAdded = true;
+                    }
+                }
+                if(!alreadyAdded){
+                    ans.push_back(nums[i]);
+                }
+            }
+        }
+
+        return ans;
+    }
+};
