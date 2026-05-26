0b85be65ca290882e5d0c17a05e60eb6e41c4289
shashikant kumar <sp8326441@gmail.com>
2026-05-26 23:41:11 +0530

    Update 12.cpp

--- array/creation/Linear_search/12.cpp
+++ array/creation/Linear_search/12.cpp
@@ -1,27 +1,17 @@
 #include<iostream>
 #include<vector>
-
 using namespace std;
-
 int main(){
-
     vector<int> nums = {4,3,2,7,8,2,3,1};
-
     int n = nums.size();
-
     vector<int> ans;
-
     for(int i = 0; i < n; i++){
-
         int freq = 0;
-
         for(int j = 0; j < n; j++){
-
             if(nums[i] == nums[j]){
                 freq++;
             }
         }
-
         if(freq == 2){
             bool alreadyAdded = false;
             for(int x : ans){
