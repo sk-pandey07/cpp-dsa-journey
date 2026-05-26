6c0ba44c932f6ee5976c4c34917a472bf5cad7f8
shashikant kumar <sp8326441@gmail.com>
2026-05-26 23:38:31 +0530

    Create 12.cpp

---
+++ array/creation/Linear_search/12.cpp
@@ -0,0 +1,50 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+int main(){
+
+    vector<int> nums = {4,3,2,7,8,2,3,1};
+
+    int n = nums.size();
+
+    vector<int> ans;
+
+    for(int i = 0; i < n; i++){
+
+        int freq = 0;
+
+        for(int j = 0; j < n; j++){
+
+            if(nums[i] == nums[j]){
+                freq++;
+            }
+        }
+
+        if(freq == 2){
+
+            bool alreadyAdded = false;
+
+            // avoid duplicate push
+            for(int x : ans){
+
+                if(x == nums[i]){
+                    alreadyAdded = true;
+                }
+            }
+
+            if(!alreadyAdded){
+                ans.push_back(nums[i]);
+            }
+        }
+    }
+
+    cout << "Duplicates: ";
+
+    for(int num : ans){
+        cout << num << " ";
+    }
+
+    return 0;
+}
