8f91f1e5034d7c58336ac070f83731cb72073aa8
shashikant kumar <sp8326441@gmail.com>
2026-05-27 23:24:30 +0530

    Update 13.cpp

--- array/creation/Linear_search/13.cpp
+++ array/creation/Linear_search/13.cpp
@@ -12,9 +12,7 @@ int main(){
     int n = nums.size();
     int count = 0;
     for(int i = 0; i < n; i++){
-
         int sum = 0;
-
         for(int j = i; j < n; j++){
             sum += nums[j];
             if(sum == k){
