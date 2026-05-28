8f1eaab93c67660858414a44b5164c5afc6deff8
shashikant kumar <sp8326441@gmail.com>
2026-05-28 23:48:44 +0530

    Update 14.cpp

--- array/creation/Linear_search/14.cpp
+++ array/creation/Linear_search/14.cpp
@@ -11,15 +11,10 @@ int main(){
     int k = 2;
 
     unordered_map<int, int> mp;
-
-    // base case
     mp[0] = 1;
-
     int prefixSum = 0;
     int count = 0;
-
     for(int i = 0; i < nums.size(); i++){
-
         prefixSum += nums[i];
         if(mp.find(prefixSum - k) != mp.end()){
             count += mp[prefixSum - k];
