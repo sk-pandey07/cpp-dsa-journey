4e387099c1d137b864ffe1d1a135ea73469c73dc
shashikant kumar <sp8326441@gmail.com>
2026-05-29 23:58:24 +0530

    Update 49.cpp

--- array/creation/Traversal/49.cpp
+++ array/creation/Traversal/49.cpp
@@ -3,10 +3,10 @@ using namespace std;
 int main(){
     int arr[] = {10, 25, 8, 40, 15};
     int n = 5;
+    
     int largest = arr[0];
     int second_largest = arr[0];
 
-    
     for(int i=1; i<n; i++){
         if(arr[i] > largest){
             second_largest = largest;
