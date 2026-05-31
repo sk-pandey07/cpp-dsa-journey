1c429fabad3dce302a6b7f15ed0fcc5c1ce8ab5e
shashikant kumar <sp8326441@gmail.com>
2026-05-31 23:50:36 +0530

    Create 51.cpp

---
+++ array/creation/Traversal/51.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int arr[] = {-5, 10, -2, 7, -1, 4};
+    int n = 6;
+
+    int positive = 0;
+    int negative = 0;
+
+    for(int i=0; i<n; i++){
+        if(arr[i] >= 0){
+            positive++;
+        }
+        else{
+            negative++;
+        }
+    }
+
+    cout << "Positive number = " << positive << endl;
+    cout << "Negative number = " << negative;
+    return 0;
+}
