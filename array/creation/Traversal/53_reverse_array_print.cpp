a495e3d38cdbf69dd07febb4af8da007488d5d40
shashikant kumar <sp8326441@gmail.com>
2026-06-01 23:41:58 +0530

    Update 53_reverse_array_print.cpp

--- array/creation/Traversal/53_reverse_array_print.cpp
+++ array/creation/Traversal/53_reverse_array_print.cpp
@@ -3,7 +3,6 @@ using namespace std;
 int main(){
     int arr[] = {4, 7, 1, 9, 2};
     int n = 5;
-    
     cout << "Reverse of array = ";
     for(int i=n-1; i>=0; i--){
         cout << arr[i] << " ";
