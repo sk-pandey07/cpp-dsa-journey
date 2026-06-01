22f55f74df9adaded4c690f815b877c70c3bd0c2
shashikant kumar <sp8326441@gmail.com>
2026-06-01 23:38:49 +0530

    Create 52.cpp

---
+++ array/creation/Traversal/52.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int arr[] = {4, 7, 1, 9, 2};
+    int n = 5;
+
+    cout << "Reverse of array = ";
+    for(int i=n-1; i>=0; i--){
+        cout << arr[i] << " ";
+    }
+    return 0;
+}
