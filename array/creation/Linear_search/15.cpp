b3aa0dd537a2d9942c2113edc1fda95d2d7c1ccd
shashikant kumar <sp8326441@gmail.com>
2026-06-02 23:23:15 +0530

    Update 15.cpp

--- array/creation/Linear_search/15.cpp
+++ array/creation/Linear_search/15.cpp
@@ -12,9 +12,7 @@ bool linearSearch(vector<int>& nums, int target){
 
     return false;
 }
-
 int main(){
-
     vector<int> nums = {100,4,200,1,3,2};
     int longest = 0;
     for(int i = 0; i < nums.size(); i++){
