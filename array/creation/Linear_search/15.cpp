9c79ceb8d36c6f42f2ac0f7e494ccd36c3e4cbcb
shashikant kumar <sp8326441@gmail.com>
2026-06-02 23:21:40 +0530

    Create 15.cpp

---
+++ array/creation/Linear_search/15.cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+bool linearSearch(vector<int>& nums, int target){
+
+    for(int num : nums){
+
+        if(num == target){
+            return true;
+        }
+    }
+
+    return false;
+}
+
+int main(){
+
+    vector<int> nums = {100,4,200,1,3,2};
+
+    int longest = 0;
+
+    for(int i = 0; i < nums.size(); i++){
+
+        int currentNum = nums[i];
+        int count = 1;
+
+        // search next consecutive numbers
+        while(linearSearch(nums, currentNum + 1)){
+
+            currentNum++;
+            count++;
+        }
+
+        longest = max(longest, count);
+    }
+
+    cout << "Longest Consecutive Length = "
+         << longest;
+
+    return 0;
+}
