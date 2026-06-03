a7803861737a1504da9f7a97e47d7fdd5cdf9460
shashikant kumar <sp8326441@gmail.com>
2026-06-03 20:42:12 +0530

    Create 54.cpp

---
+++ array/creation/Traversal/54.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int arr[] = {4, 7, 1, 9, 2};
+    float n = 5;
+    float sum = 0;
+    for(int i=0; i<n; i++){
+        sum += arr[i];
+    }
+
+    float average = sum/n;
+    cout << "average of array = " << average;
+    return 0;
+}
