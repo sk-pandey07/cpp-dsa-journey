b6dd2472f446bb00277a904aa1764af8d6f699ca
shashikant kumar <sp8326441@gmail.com>
2026-06-03 20:39:12 +0530

    Update 16_.cpp

--- array/creation/Linear_search/16_.cpp
+++ array/creation/Linear_search/16_.cpp
@@ -4,10 +4,7 @@ using namespace std;
 int main(){
     vector<int> nums = {2,3,-2,4};
     int n = nums.size();
-
     int maxProduct = nums[0];
-
-    // generate all subarrays
     for(int i = 0; i < n; i++){
         int product = 1;
         for(int j = i; j < n; j++){
