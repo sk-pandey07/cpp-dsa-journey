efa3341be9ebf32bbe6a0df0ee0f238e9455fdb3
shashikant kumar <sp8326441@gmail.com>
2026-06-04 23:41:25 +0530

    Update 55.cpp

--- array/creation/Traversal/55.cpp
+++ array/creation/Traversal/55.cpp
@@ -5,8 +5,7 @@ int main(){
     vector<int> nums = {1,2,3,1};
     int n = nums.size();
     
-    bool found = true;
-    
+    bool found = false;
     for(int i=0; i<n; i++){
         for(int j=i+1; j<n; j++){
             if(nums[i] == nums[j]){
