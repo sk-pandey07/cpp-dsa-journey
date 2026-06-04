cf8c0ca8bae4c8837ccbd34fd509bc64433cd4ba
shashikant kumar <sp8326441@gmail.com>
2026-06-04 23:39:29 +0530

    Create 55.cpp

---
+++ array/creation/Traversal/55.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int main(){
+    vector<int> nums = {1,2,3,1};
+    int n = nums.size();
+
+    bool found = true;
+    for(int i=0; i<n; i++){
+        for(int j=i+1; j<n; j++){
+            if(nums[i] == nums[j]){
+                found = true;
+                break;
+            }
+        }
+        if(found){
+            break;
+        }
+    }
+    if(found){
+        cout << "true";
+    }
+    else{
+        cout << "false";
+    }
+}
