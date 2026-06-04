fc66155dd5224f288c30e047939830c99cc80917
shashikant kumar <sp8326441@gmail.com>
2026-06-04 23:39:44 +0530

    Update 55.cpp

--- array/creation/Traversal/55.cpp
+++ array/creation/Traversal/55.cpp
@@ -4,7 +4,6 @@ using namespace std;
 int main(){
     vector<int> nums = {1,2,3,1};
     int n = nums.size();
-
     bool found = true;
     for(int i=0; i<n; i++){
         for(int j=i+1; j<n; j++){
