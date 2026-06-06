d24ffeb45da198d94e549711dcb49ed6d84c8877
shashikant kumar <sp8326441@gmail.com>
2026-06-06 23:36:31 +0530

    Update 57.cpp

--- array/creation/Traversal/57.cpp
+++ array/creation/Traversal/57.cpp
@@ -18,10 +18,8 @@ int main() {
 
     vector<int> nums1 = {1, 2, 3, 0, 0, 0};
     int m = 3;
-
     vector<int> nums2 = {2, 5, 6};
     int n = 3;
-
     sol.merge(nums1, m, nums2, n);
     cout << "Merged array: ";
     for (int num : nums1) {
