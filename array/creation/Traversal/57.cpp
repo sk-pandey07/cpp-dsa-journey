e90fd20ee673ee7e27d121a50a10fecccb88f790
shashikant kumar <sp8326441@gmail.com>
2026-06-06 23:35:38 +0530

    Create 57.cpp

---
+++ array/creation/Traversal/57.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        for (int i = 0; i < n; i++) {
+            nums1[m + i] = nums2[i];
+        }
+
+        sort(nums1.begin(), nums1.end());
+    }
+};
+
+int main() {
+    Solution sol;
+
+    vector<int> nums1 = {1, 2, 3, 0, 0, 0};
+    int m = 3;
+
+    vector<int> nums2 = {2, 5, 6};
+    int n = 3;
+
+    sol.merge(nums1, m, nums2, n);
+
+    cout << "Merged array: ";
+    for (int num : nums1) {
+        cout << num << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
