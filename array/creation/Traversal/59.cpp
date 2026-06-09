2865edba6f9797e39148b2ce5368cf43d2e6661f
shashikant kumar <sp8326441@gmail.com>
2026-06-09 23:39:33 +0530

    Update 59.cpp

--- array/creation/Traversal/59.cpp
+++ array/creation/Traversal/59.cpp
@@ -3,7 +3,6 @@ using namespace std;
 int main(){
     int arr[] = {4, 7, 1, 9, 2};
     int n = 5;
-
     int largest_element = arr[0];
     for(int i=0; i<n; i++){
         if(arr[i] > largest_element){
