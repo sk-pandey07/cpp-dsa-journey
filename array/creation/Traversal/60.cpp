bda7ab8465eb2efda9f7ede9cd47839dd02e1177
shashikant kumar <sp8326441@gmail.com>
2026-06-09 23:43:13 +0530

    Create 60.cpp

---
+++ array/creation/Traversal/60.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+int main(){
+
+    vector<int> nums = {3,2,2,3};
+
+    int val = 3;
+
+    int j = 0;
+
+    for(int i = 0; i < nums.size(); i++){
+
+        if(nums[i] != val){
+
+            nums[j] = nums[i];
+            j++;
+        }
+    }
+
+    cout << "Remaining Elements: ";
+
+    for(int i = 0; i < j; i++){
+        cout << nums[i] << " ";
+    }
+    cout << "\nCount = " << j;
+    return 0;
+}
