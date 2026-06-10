e335fab22845b9561260fde717ce9d794999bf58
shashikant kumar <sp8326441@gmail.com>
2026-06-10 23:46:46 +0530

    Create 61.cpp

---
+++ array/creation/Traversal/61.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] >= target) {
+                return i;
+            }
+        }
+
+        return nums.size();
+    }
+};
+
+int main() {
+    Solution sol;
+
+    vector<int> nums = {1, 3, 5, 6};
+    int target = 5;
+
+    int result = sol.searchInsert(nums, target);
+
+    cout << "Index: " << result << endl;
+
+    return 0;
+}
