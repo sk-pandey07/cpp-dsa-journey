655fde4f189c34a9f2497078639bd3e1612787a7
shashikant kumar <sp8326441@gmail.com>
2026-06-12 23:43:33 +0530

    Update 62.cpp

--- array/creation/Traversal/62.cpp
+++ array/creation/Traversal/62.cpp
@@ -9,9 +9,7 @@ int main(){
 
     int currSum = 0;
     int maxSum = nums[0];
-
     for(int i = 0; i < nums.size(); i++){
-
         currSum += nums[i];
         maxSum = max(maxSum, currSum);
         if(currSum < 0){
