3a83ec4132312e1c6f83c0cab6febdc51f6e9450
shashikant kumar <sp8326441@gmail.com>
2026-06-12 23:42:35 +0530

    Create 62.cpp

---
+++ array/creation/Traversal/62.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+int main(){
+
+    vector<int> nums = {-2,1,-3,4,-1,2,1,-5,4};
+
+    int currSum = 0;
+    int maxSum = nums[0];
+
+    for(int i = 0; i < nums.size(); i++){
+
+        currSum += nums[i];
+
+        maxSum = max(maxSum, currSum);
+
+        if(currSum < 0){
+            currSum = 0;
+        }
+    }
+
+    cout << "Maximum Subarray Sum = "
+         << maxSum;
+
+    return 0;
+}
