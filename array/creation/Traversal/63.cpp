7325f2e43249d7717cbe49f8ca66bcac417ef39c
shashikant kumar <sp8326441@gmail.com>
2026-06-13 22:51:43 +0530

    Create 63.cpp

---
+++ array/creation/Traversal/63.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+int main(){
+
+    vector<int> digits = {1,2,3};
+
+    int n = digits.size();
+
+    for(int i = n - 1; i >= 0; i--){
+
+        if(digits[i] < 9){
+
+            digits[i]++;
+
+            for(int digit : digits){
+                cout << digit << " ";
+            }
+
+            return 0;
+        }
+
+        digits[i] = 0;
+    }
+
+    digits.insert(digits.begin(), 1);
+
+    for(int digit : digits){
+        cout << digit << " ";
+    }
+
+    return 0;
+}
