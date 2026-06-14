f8071a55b01f82322b9e9250072e7e96e5064463
shashikant kumar <sp8326441@gmail.com>
2026-06-14 23:50:19 +0530

    Create 64.cpp

---
+++ array/creation/Traversal/64.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int maxProfit = 0;
+
+        for (int i = 0; i < prices.size(); i++) {
+            for (int j = i + 1; j < prices.size(); j++) {
+                int profit = prices[j] - prices[i];
+                maxProfit = max(maxProfit, profit);
+            }
+        }
+
+        return maxProfit;
+    }
+};
+
+int main() {
+    Solution obj;
+
+    vector<int> prices = {7, 1, 5, 3, 6, 4};
+
+    cout << "Maximum Profit = " << obj.maxProfit(prices) << endl;
+
+    return 0;
+}
