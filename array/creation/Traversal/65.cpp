c84cf6444dc0c58bec291da998cbe5600f7d0ac4
shashikant kumar <sp8326441@gmail.com>
2026-06-15 23:30:28 +0530

    Update 65.cpp

--- array/creation/Traversal/65.cpp
+++ array/creation/Traversal/65.cpp
@@ -1,13 +1,10 @@
 #include <iostream>
 #include <vector>
 using namespace std;
-
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-
         int n = nums.size();
-
         for(int i = 0; i < n; i++) {
             int freq = 0;
             for(int j = 0; j < n; j++) {
