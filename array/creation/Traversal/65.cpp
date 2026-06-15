0db2f3f1a8d57d198937f3698871c4905e9205f5
shashikant kumar <sp8326441@gmail.com>
2026-06-15 23:30:12 +0530

    Update 65.cpp

--- array/creation/Traversal/65.cpp
+++ array/creation/Traversal/65.cpp
@@ -9,11 +9,8 @@ public:
         int n = nums.size();
 
         for(int i = 0; i < n; i++) {
-
             int freq = 0;
-
             for(int j = 0; j < n; j++) {
-
                 if(nums[i] == nums[j]) {
                     freq++;
                 }
