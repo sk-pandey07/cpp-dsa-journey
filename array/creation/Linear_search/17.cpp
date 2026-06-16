11b1b5a0b8c4da5f633d9c4d4cddfa206e402ee5
shashikant kumar <sp8326441@gmail.com>
2026-06-16 23:19:06 +0530

    Update 17.cpp

--- array/creation/Linear_search/17.cpp
+++ array/creation/Linear_search/17.cpp
@@ -1,11 +1,9 @@
 #include <iostream>
 #include <vector>
 using namespace std;
-
 class Solution {
 public:
     vector<int> twoSum(vector<int>& numbers, int target) {
-
         int n = numbers.size();
         for(int i = 0; i < n; i++) {
             for(int j = i + 1; j < n; j++) {
