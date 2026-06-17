727cef0b55279a5d03afc5bb09557b63d8744b45
shashikant kumar <sp8326441@gmail.com>
2026-06-17 23:56:54 +0530

    Update 18.cpp

--- array/creation/Linear_search/18.cpp
+++ array/creation/Linear_search/18.cpp
@@ -1,15 +1,11 @@
 #include <iostream>
 #include <vector>
 using namespace std;
-
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {
-
         int n = nums.size();
-
         for(int i = 0; i <= n; i++) {
-
             bool found = false;
             for(int j = 0; j < n; j++) {
                 if(nums[j] == i) {
