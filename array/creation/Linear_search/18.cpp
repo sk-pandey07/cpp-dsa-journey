ecb723022602c858c75f7bd6b7109375586ee01c
shashikant kumar <sp8326441@gmail.com>
2026-06-17 23:56:36 +0530

    Update 18.cpp

--- array/creation/Linear_search/18.cpp
+++ array/creation/Linear_search/18.cpp
@@ -11,20 +11,16 @@ public:
         for(int i = 0; i <= n; i++) {
 
             bool found = false;
-
             for(int j = 0; j < n; j++) {
-
                 if(nums[j] == i) {
                     found = true;
                     break;
                 }
             }
-
             if(!found) {
                 return i;
             }
         }
-
         return -1;
     }
 };
