3045fc61b3d7bae62e5304e65eb55e9ef3947e77
shashikant kumar <sp8326441@gmail.com>
2026-06-18 23:30:36 +0530

    Create 66.cpp

---
+++ array/creation/Traversal/66.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int n = nums.size();
+
+        int expectedSum = n * (n + 1) / 2;
+        int actualSum = 0;
+
+        for (int num : nums) {
+            actualSum += num;
+        }
+
+        return expectedSum - actualSum;
+    }
+};
+
+int main() {
+    vector<int> nums = {3, 0, 1};
+
+    Solution obj;
+    cout << "Missing Number: " << obj.missingNumber(nums) << endl;
+
+    return 0;
+}
