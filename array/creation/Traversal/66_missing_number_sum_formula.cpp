8d25f4b4d0aead039ae628d378872a56eadceefb
shashikant kumar <sp8326441@gmail.com>
2026-06-18 23:32:54 +0530

    Update 66_missing_number_sum_formula.cpp

--- array/creation/Traversal/66_missing_number_sum_formula.cpp
+++ array/creation/Traversal/66_missing_number_sum_formula.cpp
@@ -1,28 +1,22 @@
 #include <iostream>
 #include <vector>
-using namespace std;
 
+using namespace std;
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {
         int n = nums.size();
-
         int expectedSum = n * (n + 1) / 2;
         int actualSum = 0;
-
         for (int i = 0; i < nums.size(); i++) {
             actualSum += nums[i];
         }
-
         return expectedSum - actualSum;
     }
 };
-
 int main() {
     vector<int> nums = {3, 0, 1};
-
     Solution obj;
     cout << "Missing Number: " << obj.missingNumber(nums) << endl;
-
     return 0;
 }
