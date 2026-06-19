583a606191730f68c9690c3f77ffe2137e855c87
shashikant kumar <sp8326441@gmail.com>
2026-06-19 22:56:32 +0530

    Update 67.cpp

--- array/creation/Traversal/67.cpp
+++ array/creation/Traversal/67.cpp
@@ -5,31 +5,23 @@ using namespace std;
 class Solution {
 public:
     int pivotIndex(vector<int>& nums) {
-
         int n = nums.size();
-
         for(int i = 0; i < n; i++) {
-
             int leftSum = 0;
             int rightSum = 0;
-
             for(int j = 0; j < i; j++) {
                 leftSum += nums[j];
             }
-
             for(int j = i + 1; j < n; j++) {
                 rightSum += nums[j];
             }
-
             if(leftSum == rightSum) {
                 return i;
             }
         }
-
         return -1;
     }
 };
-
 int main() {
     vector<int> nums = {1, 7, 3, 6, 5, 6};
 
