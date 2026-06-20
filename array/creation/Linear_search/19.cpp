4aa32d4d758bc6cee8de6a121f1e6dd555ac07d5
shashikant kumar <sp8326441@gmail.com>
2026-06-20 23:40:32 +0530

    Create 19.cpp

---
+++ array/creation/Linear_search/19.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+int main(){
+
+    vector<int> nums = {1,7,3,6,5,6};
+
+    int totalSum = 0;
+
+    for(int num : nums){
+        totalSum += num;
+    }
+
+    int leftSum = 0;
+
+    for(int i = 0; i < nums.size(); i++){
+
+        int rightSum = totalSum - leftSum - nums[i];
+
+        if(leftSum == rightSum){
+
+            cout << "Pivot Index = "
+                 << i;
+
+            return 0;
+        }
+
+        leftSum += nums[i];
+    }
+
+    cout << -1;
+
+    return 0;
+}
