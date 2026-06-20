44db5b63c24ad9e8da50b8a33c6e96d7739abe17
shashikant kumar <sp8326441@gmail.com>
2026-06-20 23:41:39 +0530

    Update 19.cpp

--- array/creation/Linear_search/19.cpp
+++ array/creation/Linear_search/19.cpp
@@ -2,19 +2,13 @@
 #include<vector>
 
 using namespace std;
-
 int main(){
-
     vector<int> nums = {1,7,3,6,5,6};
-
     int totalSum = 0;
-
     for(int num : nums){
         totalSum += num;
     }
-
     int leftSum = 0;
-
     for(int i = 0; i < nums.size(); i++){
         int rightSum = totalSum - leftSum - nums[i];
         if(leftSum == rightSum){
