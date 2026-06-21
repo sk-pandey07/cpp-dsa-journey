1859e8fe3482ed943c4ae8b0ad6f2c36c5c0d635
shashikant kumar <sp8326441@gmail.com>
2026-06-21 23:48:31 +0530

    Update 68.cpp

--- array/creation/Traversal/68.cpp
+++ array/creation/Traversal/68.cpp
@@ -6,17 +6,13 @@ using namespace std;
 class Solution {
 public:
     vector<int> sortedSquares(vector<int>& nums) {
-
         for(int i = 0; i < nums.size(); i++) {
             nums[i] = nums[i] * nums[i];
         }
-
         sort(nums.begin(), nums.end());
-
         return nums;
     }
 };
-
 int main() {
     Solution obj;
     vector<int> nums = {-4, -1, 0, 3, 10};
