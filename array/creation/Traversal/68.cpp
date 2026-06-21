0b9776d17048889e7cf32f4f7e69c022d750774a
shashikant kumar <sp8326441@gmail.com>
2026-06-21 23:47:24 +0530

    Create 68.cpp

---
+++ array/creation/Traversal/68.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> sortedSquares(vector<int>& nums) {
+
+        for(int i = 0; i < nums.size(); i++) {
+            nums[i] = nums[i] * nums[i];
+        }
+
+        sort(nums.begin(), nums.end());
+
+        return nums;
+    }
+};
+
+int main() {
+    Solution obj;
+    vector<int> nums = {-4, -1, 0, 3, 10};
+
+    vector<int> result = obj.sortedSquares(nums);
+
+    for(int x : result) {
+        cout << x << " ";
+    }
+
+    return 0;
+}
