3bf3a4a9a49f0852aeb3d6663d4c7451596da78d
shashikant kumar <sp8326441@gmail.com>
2026-06-22 23:29:19 +0530

    Update 01.cpp

--- array/creation/two pointer/01.cpp
+++ array/creation/two pointer/01.cpp
@@ -14,14 +14,10 @@ int main(){
     int left = 0;
     int right = n - 1;
     int index = n - 1;
-
     while(left <= right){
-
         int leftSquare = nums[left] * nums[left];
         int rightSquare = nums[right] * nums[right];
-
         if(leftSquare > rightSquare){
-
             ans[index] = leftSquare;
             left++;
         }
