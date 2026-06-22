2b4f18e0ad836dde375d8d2d4b6d3e33bc954b83
shashikant kumar <sp8326441@gmail.com>
2026-06-22 23:28:09 +0530

    Create 01.cpp

---
+++ array/creation/two pointer/01.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+int main(){
+
+    vector<int> nums = {-4,-1,0,3,10};
+
+    int n = nums.size();
+
+    vector<int> ans(n);
+
+    int left = 0;
+    int right = n - 1;
+    int index = n - 1;
+
+    while(left <= right){
+
+        int leftSquare = nums[left] * nums[left];
+        int rightSquare = nums[right] * nums[right];
+
+        if(leftSquare > rightSquare){
+
+            ans[index] = leftSquare;
+            left++;
+        }
+        else{
+
+            ans[index] = rightSquare;
+            right--;
+        }
+
+        index--;
+    }
+
+    cout << "Result: ";
+
+    for(int num : ans){
+        cout << num << " ";
+    }
+
+    return 0;
+}
