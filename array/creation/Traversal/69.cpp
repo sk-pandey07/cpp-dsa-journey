008dac7b5df9504ccf92f06bd49d610f7a65573a
shashikant kumar <sp8326441@gmail.com>
2026-06-23 23:34:58 +0530

    Update 69.cpp

--- array/creation/Traversal/69.cpp
+++ array/creation/Traversal/69.cpp
@@ -7,7 +7,6 @@ class Solution {
 public:
     int thirdMax(vector<int>& nums) {
         sort(nums.begin(), nums.end());
-
         nums.erase(unique(nums.begin(), nums.end()), nums.end());
         int n = nums.size();
         if (n >= 3) {
