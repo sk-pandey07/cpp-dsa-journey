17fb45585936e1510246e7510b57af67c70906bd
shashikant kumar <sp8326441@gmail.com>
2026-06-24 23:18:42 +0530

    Create 70.cpp

---
+++ array/creation/Traversal/70.cpp
@@ -0,0 +1,50 @@
+#include<iostream>
+#include<vector>
+#include<climits>
+
+using namespace std;
+
+int main(){
+
+    vector<int> nums = {2,2,3,1};
+
+    long first = LONG_MIN;
+    long second = LONG_MIN;
+    long third = LONG_MIN;
+
+    for(int num : nums){
+
+        if(num == first ||
+           num == second ||
+           num == third){
+            continue;
+        }
+
+        if(num > first){
+
+            third = second;
+            second = first;
+            first = num;
+        }
+
+        else if(num > second){
+
+            third = second;
+            second = num;
+        }
+
+        else if(num > third){
+
+            third = num;
+        }
+    }
+
+    if(third == LONG_MIN){
+        cout << first;
+    }
+    else{
+        cout << third;
+    }
+
+    return 0;
+}
